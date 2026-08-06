7e8acbefeaf6dd134bab6c2b9a98db2d5a3f5279
Vitor Vianna Magaldi <vvm2@cesar.school>
2026-08-06 01:52:01 -0300

    atualizacao exercicio

--- teste.py
+++ teste.py
@@ -1,1 +1,10 @@
-print ("Hello, World!")
+numero = 0
+soma = 0
+
+for numero in range(1,6):
+    numero = float(input(f"Digite o {numero}º número: "))
+    soma += numero
+
+media = soma / 5
+
+print(f"A média dos numeros digitados é: {media:.2f}")
